1b0bccb4f18baecc4c36efc28276d07b8a36468a
mariojguerra <mjpguerra@icloud.com>
2025-04-08 19:26:21 -0300

    nicpacks

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 # config.py
-import os
 
-# Chave secreta
 SECRET_KEY = '2404@Theo'
-
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'hvMYogfEVMNDAKMrrxazEALyySAKgeYU'
 MYSQL_HOST = 'mysql.railway.internal'
 MYSQL_PORT = 3306
 MYSQL_DB = 'railway'
-
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
