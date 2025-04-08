2d32ed72a3ce330f07ee52031d58aeaa031741ea
mariojguerra <mjpguerra@icloud.com>
2025-04-08 12:24:47 -0300

    nicpacks

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class ImagemAnimal(db.Model):
     __tablename__ = 'imagens_animais'
 
     id = db.Column(db.Integer, primary_key=True)
-    animal_id = db.Column(db.String, db.ForeignKey('animais.id'), nullable=False)
+    animal_id = db.Column(db.String, db.ForeignKey('animal.id'), nullable=False)
     url = db.Column(db.String, nullable=False)
     data_upload = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -25,8 +25,6 @@ class ImagemAnimal(db.Model):
         }
 
 class Animal(db.Model):
-    __tablename__ = 'animais'
-
     id = db.Column(db.Integer, primary_key=True)
     id_animal = db.Column(db.String(50), nullable=False)
     nome = db.Column(db.String(100), nullable=False)
