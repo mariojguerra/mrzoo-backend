ec27412d8f2dcca79783687a9bb2a374267e348b
mariojguerra <mjpguerra@icloud.com>
2025-04-08 20:23:09 -0300

    nicpacks

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 SECRET_KEY = '2404@Theo'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'hvMYogfEVMNDAKMrrxazEALyySAKgeYU'
-MYSQL_HOST = 'mysql.railway.internal'
-MYSQL_PORT = 3306
+MYSQL_HOST = 'caboose.proxy.rlwy.net'
+MYSQL_PORT = 43162
 MYSQL_DB = 'railway'
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 #mysql://root:hvMYogfEVMNDAKMrrxazEALyySAKgeYU@mysql.railway.internal:3306/railway
-
+#mysql://root:hvMYogfEVMNDAKMrrxazEALyySAKgeYU@caboose.proxy.rlwy.net:43162/railway
