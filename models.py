a5bf98e660093352b6ab23fd9004f75f0981543e
mariojguerra <mjpguerra@icloud.com>
2025-04-08 12:21:18 -0300

    nicpacks

--- models.py
+++ models.py
@@ -25,6 +25,8 @@ class ImagemAnimal(db.Model):
         }
 
 class Animal(db.Model):
+    __tablename__ = 'animais'
+
     id = db.Column(db.Integer, primary_key=True)
     id_animal = db.Column(db.String(50), nullable=False)
     nome = db.Column(db.String(100), nullable=False)
