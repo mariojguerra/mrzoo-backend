b47b85e37f3297c4d438ef5ffe2b1886e8330bb2
mariojguerra <mjpguerra@icloud.com>
2025-04-09 10:55:56 -0300

    nicpacks

--- routes/__init__.py
+++ routes/__init__.py
@@ -11,7 +11,7 @@ from .uploads import upload_routes
 from .match_routes import match_routes
 from .chat_routes import chat_routes
 from .notificacao_routes import notificacao_routes
-from .notificacao_routes import notificacao_routes
+from .like_routes import like_routes
 
 routes = Blueprint('routes', __name__)
 socketio = SocketIO()
@@ -25,5 +25,5 @@ routes.register_blueprint(animal_routes)
 routes.register_blueprint(upload_routes, url_prefix="/uploads")
 routes.register_blueprint(match_routes)
 routes.register_blueprint(chat_routes)
-routes.register_blueprint(chat_routes)
-routes.register_blueprint(chat_routes)
+routes.register_blueprint(notificacao_routes)
+routes.register_blueprint(like_routes)
