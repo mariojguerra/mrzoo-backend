3b8e7db9cd8de1725b40d2a5cffaf923215d3c1b
mariojguerra <mjpguerra@icloud.com>
2025-04-08 23:16:24 -0300

    nicpacks

---
+++ cloudinary_config.py
@@ -0,0 +1,8 @@
+import cloudinary
+import cloudinary.uploader
+
+cloudinary.config(
+  cloud_name = "dd9ilzlal",
+  api_key = "162884696795991",
+  api_secret = "vx8uO6Wa--Ri5djCFvf7-CS0_Bs"
+)
--- routes_api.py
+++ routes_api.py
@@ -11,6 +11,9 @@ import os
 from flask import send_from_directory, abort
 from datetime import datetime
 import uuid
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
+from cloudinary_config import cloudinary
 
 UPLOAD_FOLDER = 'static/uploads/images'
 socketio = SocketIO()
@@ -201,6 +204,76 @@ def upload_imagens_animal():
     except Exception as e:
         print(f"Erro ao adicionar imagens: {e}")
         return jsonify({"message": str(e)}), 500
+
+@routes.route("/upload_cloudinary", methods=["POST"])
+@jwt_required()
+def upload_para_cloudinary():
+    usuario_id = get_jwt_identity()
+
+    if 'imagem' not in request.files or 'animal_id' not in request.form:
+        return jsonify({"message": "Imagem e animal_id são obrigatórios"}), 400
+
+    imagem = request.files['imagem']
+    animal_id = request.form['animal_id']
+
+    try:
+        resultado = upload(imagem, folder=f"mrzoo/usuario_{usuario_id}//animal_{animal_id}/")
+        url = resultado.get("secure_url")
+
+        animal = Animal.query.filter_by(id=animal_id, usuario_id=usuario_id).first()
+
+        if not animal:
+           return jsonify({"message": "Animal não encontrado ou não pertence ao usuário!"}), 404
+
+        # Pegando a URL direto do campo do form
+        animal.imagem_url = url
+        db.session.commit()
+
+        return jsonify({"url": url}), 200
+    except Exception as e:
+        print(f"Erro ao enviar para o Cloudinary: {e}")
+        return jsonify({"erro": "Falha no upload"}), 500
+
+
+@routes.route("/upload_cloudinary_multiplas", methods=["POST"])
+@jwt_required()
+def upload_multiplas_cloudinary():
+    usuario_id = get_jwt_identity()
+    animal_id = request.form.get("animal_id")
+
+    if not animal_id:
+        return jsonify({"erro": "animal_id é obrigatório"}), 400
+
+    arquivos = request.files.getlist("imagens")
+    if not arquivos:
+        return jsonify({"erro": "Nenhuma imagem enviada"}), 400
+
+    urls_salvas = []
+
+    try:
+        for imagem in arquivos:
+            if imagem:
+                resultado = upload(
+                    imagem,
+                    folder=f"mrzoo/usuario_{usuario_id}/animal_{animal_id}/",
+                    public_id=f"{animal_id}_{uuid.uuid4().hex[:6]}"
+                )
+                url = resultado.get("secure_url")
+                urls_salvas.append(url)
+
+                nova_imagem = ImagemAnimal(
+                    animal_id=animal_id,
+                    url=url,
+                    data_upload=datetime.utcnow()
+                )
+                db.session.add(nova_imagem)
+
+        db.session.commit()
+        return jsonify({"mensagem": "Imagens salvas com sucesso", "imagens": urls_salvas}), 200
+
+    except Exception as e:
+        print(f"Erro ao enviar para Cloudinary: {e}")
+        return jsonify({"erro": "Erro interno no servidor"}), 500
 
 
 @routes.route("/upload_imagens_principal", methods=["POST"])
