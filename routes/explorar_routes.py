01eb820523a26f810154d09685b5acecb1bad31a
mariojguerra <mjpguerra@icloud.com>
2025-04-15 13:56:43 -0300

    nicpacks

--- routes/explorar_routes.py
+++ routes/explorar_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from models import Animal, db
-from sqlalchemy import or_
+from models import Animal
 
 explorar_routes = Blueprint('explorar_routes', __name__)
 
